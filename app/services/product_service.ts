bb7f5d6d9693e66a3638a8c8d4a1ac9ac0ce6df1
vitaum <gvictor567@gmail.com>
2024-05-29 02:15:55 -0300

    initial

---
+++ app/services/product_service.ts
@@ -0,0 +1,21 @@
+export default class ProductsService {
+
+    public async productsData(data: any) {
+
+        const products = data.products.data;
+        let refactorProducts : any[] = [];
+
+        products.forEach((product: any) => {
+            refactorProducts.push({
+                "id": product.id,
+                "shop_id": product.shop_id,
+                "title": product.title,
+                "price": product.product_default_variant.price,
+                "compare_at_price": product.product_default_variant.compare_at_price,
+                "url_image": product.images[0].url.replace("/", ""),
+            });
+        });
+
+        return refactorProducts;
+    }
+}
